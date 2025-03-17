687888de82e97091a2e5550a35bac1d323d81499
harsh <harsh.chouhan010@gmail.com>
2025-03-17 08:37:38 -0700

    new

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,18 @@
 import Header3 from "../component/header";
 import Hero1 from "../component/home/hero1";
-
+import Categories from "../component/home/Categories";
+import Collections from "../component/home/Collections";
+import Hotbids from "../component/home/Hotbids";
+import Process from "../component/home/Process";
 export default function Home() {
     return (
         <>
             <Header3 />
             <Hero1 />
+            <Hotbids />
+        <Collections />
+        <Categories />
+        <Process />
             <div class="button-box">
                 <div class="liquid"></div>
             </div>
