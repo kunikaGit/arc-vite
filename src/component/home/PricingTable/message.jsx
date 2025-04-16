5353efc14fe343ca10d87b27e5a8eb31ced279c8
kunikaesp <kunika@espsofttech.com>
2025-04-17 00:59:33 +0530

    modal css change

--- src/component/home/PricingTable/message.jsx
+++ src/component/home/PricingTable/message.jsx
@@ -13,11 +13,13 @@ const useMessageData = () => {
         switch (type) {
             case "minTrade":
                 content =
-                    `During each stage of our evaluation, there are a minimum of 3 trading days. A day is counted as a trading day if at least one trade is executed. 
-                    Holding a trade over multiple days will not count as multiple trading days.\n \n
-                    Note:
-                    If you reach the profit target before completing the 3 minimum trading days, 
-                    it is allowed to execute 0.01 lot trades on the remaining days.`
+                    <div>
+                        <p>During each stage of our evaluation, there are a minimum of 3 trading days. A day is counted as a trading day if at least one trade is executed.
+                            Holding a trade over multiple days will not count as multiple trading days.</p>
+                        <p> <strong>Note:</strong></p>
+                        <p> If you reach the profit target before completing the 3 minimum trading days,
+                            it is allowed to execute 0.01 lot trades on the remaining days.</p>
+                    </div>
                 break;
             case "dailyLoss":
                 content =
@@ -25,7 +27,7 @@ const useMessageData = () => {
                         <p>The Maximum Daily Loss is the amount you are allowed to lose every day.</p>
                         <p>For the purpose of this rule, the higher value between equity and balance will be used. This rule is set as a % of the starting equity or balance of every day. The rule states that the equity of the day, which is the result of the currently floating PnL (Profit and Loss) in sum with all closed positions of that day must not hit the Maximum Daily Loss Limit.</p>
                         <p>
-                            <strong>Example:</strong>
+                            <strong>Example:</strong><br />
                             You have a master account. At the start of day 5, your account balance is $105.000 and your equity is $107.000. The Daily Loss Limit is 5% from the starting equity for this example, this means that your equity on day 5 can't go lower than: Maximum Daily loss = $107.000 starting equity balance * 5% Daily Loss Limit = $5.350 Daily Loss Limit. $107.000 starting equity balance on day 5 - $5.350 Daily Loss Limit= $101.650 If your equity goes below $101.650 at any certain moment of time on day 5 your account will be closed.
                         </p>
                     </div>
