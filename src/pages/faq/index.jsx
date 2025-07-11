797567b8beb25da0bf32aebc9fdbcc10d0bc7760
kunikaesp <kunika@espsofttech.com>
2025-07-12 00:34:31 +0530

    design

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import faqData from "./questions.jsx";
 import Header2 from "../../component/home2/header2";
 import Footer1 from "../../component/footer";
+import imageMap from "../../utlis/helper.js";
 
 const Faq = () => {
     const faqCategories = Object.keys(faqData);
@@ -25,18 +26,9 @@ const Faq = () => {
         <>
             <div className="dark-for-header relative">
                 <Header2 />
-
                 <section className="relative pt-4">
                     <div className="absolute inset-0  overflow-hidden  -z-10 common-banner" style={{ height: '320px' }} >
-                        <video
-                            ref={videoRef}
-                            autoPlay
-                            loop
-                            muted
-                            playsInline
-                            className="w-full object-cover  -z-10 h-full "
-                            src="video/graphs.mp4"
-                        />
+                        <img  src={imageMap['glob-bg.jpg']} className="w-full object-cover  -z-10 h-full "/>
                     </div>
                 </section>
                 <div className="text-center flex flex-col items-center justify-center m-auto xl:max-w-4xl lg:max-w-full lg:text-6xl md:text-4xl sm:text-3xl"
@@ -64,7 +56,7 @@ const Faq = () => {
                     </form>
                 </div>
 
-                <section className="relative py-10 bg-white dark:bg-gradient">
+                <section className="relative py-10">
                     <div className="container mx-auto px-4">
 
                         <div className="flex flex-col lg:flex-row lg:space-x-10">
@@ -72,24 +64,24 @@ const Faq = () => {
                                 <div key={colIndex} className="lg:w-1/2">
                                     {column.map((category, index) => (
                                         <div key={index} className="mb-10">
-                                            <h2 className="text-lg font-semibold mb-5 flex items-center gap-3 text-jacarta-700 dark:text-white capitalize">
+                                            <h2 className="text-lg font-semibold mb-5 flex items-center gap-3 text-white capitalize">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={24} width={24}>
                                                     <path d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM12 11H8V13H12V16L16 12L12 8V11Z"></path>
                                                 </svg>
                                                 {category.replace(/([A-Z])/g, " $1")}
                                             </h2>
                                             {faqData[category].map((elm) => (
-                                                <div key={elm.id} className="mb-4 overflow-hidden border rounded-lg border-gray-200 dark:border-jacarta-600">
+                                                <div key={elm.id} className="mb-4 overflow-hidden border rounded-lg border-jacarta-600">
                                                     <button
-                                                        className="flex justify-between w-full px-4 py-3 text-left font-medium text-gray-700 dark:bg-jacarta-700 dark:text-white"
+                                                        className="flex justify-between w-full px-4 py-3 text-left font-medium bg-jacarta-700 text-white"
                                                         onClick={() => toggleAccordion(elm.id)}
                                                     >
                                                         <span>{elm.question}</span>
                                                         <span className="text-xl">{openQuestion === elm.id ? "−" : "+"}</span>
                                                     </button>
                                                     {openQuestion === elm.id && (
-                                                        <div className="border-t border-gray-200 bg-white p-4 dark:border-jacarta-600 dark:bg-jacarta-700">
-                                                            <p className="text-jacarta-500 dark:text-jacarta-300">{elm.answer}</p>
+                                                        <div className="border-t border-gray-200 p-4 border-jacarta-600 bg-jacarta-700">
+                                                            <p className="text-jacarta-500 text-white">{elm.answer}</p>
                                                         </div>
                                                     )}
                                                 </div>
